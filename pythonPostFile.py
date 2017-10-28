60d320934d264c983a680a37979c27be38d540d7
Aditya Inapurapu <adityaii@gmail.com>
2017-10-28 09:29:50 -0700

    Initial commit

---
+++ pythonPostFile.py
@@ -0,0 +1,10 @@
+import requests
+import sys
+
+def main():
+	url = 'https://www.google.com/search?q=test&oq=test'
+	files = {'data': open('requirements.txt', 'rb')}
+	r = requests.post(url, files=files)
+	print (r.status_code)
+
+main()
